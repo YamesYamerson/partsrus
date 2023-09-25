4e749bb199f46fdb4ec92ad769f96fd28d56b202
mclean1 <mclean1@cs.dal.ca>
2023-09-25 16:17:49 -0300

    Commented and organized code

--- php/signin_form.php
+++ php/signin_form.php
@@ -2,11 +2,19 @@
 // Accesses MAMP server and MySQL server and accesses
 require_once 'php/server_login.php';
 // Initialize variables
+// Errors array
 $errors = array (
     'client_id_error' => '',
     'client_username_error' => '',
     'password_error' => '',
     );
+// Error handling boolean values
+$id_valid = FALSE;
+$id_exists = FALSE;
+$username_valid = FALSE;
+$username_exists = FALSE;
+$password_valid = FALSE;
+$password_verified = FALSE;
 // Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Form submitted variables with whitespace removed
@@ -19,21 +27,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $password_length = strlen($password_entry);
 }
 // Create a prepared statement to get data from the clients771 table
-$stmt = $conn->prepare("SELECT clientId771, clientName771, clientCompPassword771, username771 FROM clients771");
+$stmt = $conn->prepare("SELECT clientId771, clientCompPassword771, username771 FROM clients771");
 $stmt->execute();
 $stmt->store_result();
-$stmt->bind_result($client_id, $client_name, $password, $username);
-// Error handling boolean values
-$id_valid = FALSE;
-$id_exists = FALSE;
-$username_valid = FALSE;
-$username_exists = FALSE;
-$password_valid = FALSE;
-$password_verified = FALSE;
-
+$stmt->bind_result($client_id, $password, $username);
 // Loop through users to check for existing usernames and passwords
 while ($stmt->fetch()) {
-    if(empty($client_id_entry)){
+    // Checks to see if Client Username entry field is empty
+    if(empty($client_name_entry)){
+        // Checks if Client ID entry is valid
+        if($id_valid === FALSE && $client_id_length == 5){
+            $id_valid = TRUE;
+        }
+        // Checks to see if Client ID entry is in DB if ID is valid
+        if ($id_valid === TRUE) {
+            if($client_id_entry == $client_id){
+                $id_exists = TRUE;
+            // Sends client error message if Client ID is not found
+            }else{
+                $errors['client_id_error'] = "Client ID not found!";
+                break;
+            }
+        }
+    }
+    // Checks to see if Client ID field is empty
+    if(!isset($client_id_entry) === TRUE){
         // Checks to see if Client Username entry is valid
         if($username_valid === FALSE && ($client_name_length < 1 || $client_name_length > 30)){
             $errors['client_username_error'] = "Client Username must be 1-30 characters!";
@@ -42,33 +60,17 @@ while ($stmt->fetch()) {
         // Checks to see if Client Username entry is in DB
         if($client_name_entry == $username){
             $username_exists = TRUE;
-            echo "username exists!!!";
-        }
-    }
-    if(empty($client_name_entry)){
-        // Checks if Client ID entry is valid
-        if($id_valid === FALSE && $client_id_length == 5){
-            $id_valid = TRUE;
-        }else{
-            $errors['client_id_error'] = "Client ID must be 5 digits!";
-            break;
-        }
-        // Checks to see if Client ID entry is in DB
-        if ($id_valid === TRUE) {
-            if($client_id_entry == $client_id){
-                $id_exists = TRUE;
-            }else{
-                $errors['client_id_error'] = "Client id not found!";
-            }
         }
     }
     // Checks to see if use has input text for ClientID and ClientUsername (unsupported action)
-    if(!empty($client_id_entry && !empty($client_name_entry))){
+    if(!empty($client_id_entry) === TRUE && !empty($client_name_entry)){
         unset($errors);
         $errors['client_id_error'] = "Enter text in one field ONLY: Client Id or Client Username";
+        break;
     }
-    // Checks to see if password is valid format
+    // Checks to see if either Client ID or Username exist in database and have been confirmed
     if($id_exists === TRUE || $username_exists === TRUE){
+            // Checks to see if password is valid format
         if ($password_length >= 7 && $password_length <= 20) {
             $password_valid = TRUE;
         }else{
@@ -82,22 +84,25 @@ while ($stmt->fetch()) {
         if(password_verify($password_entry, $password)) {
             $password_verified = TRUE;
             session_start(); // Start the session here
+            // Set Session Variables
             $_SESSION["signin"] = TRUE;
             $_SESSION["client_id"] = $client_id;
             header('Location: index.php');
             exit; // Exit to prevent further script execution
+        // Exits loop if password for detected client ID or username exists
         }else{
             $errors['password_error'] = "Incorrect Password!"; // Error message if password is incorrect
-
             break;
         }
     }
 }
-// Outputs error message if username is not in database
-if($username_exists == FALSE ){
-    $errors['client_username_error'] = "Username not found!";
 
+//If client id entry is more or less than 5 digits, display error message
+if($id_valid === FALSE && empty($client_name_entry) === TRUE
+){
+    $errors['client_id_error'] = "Client ID must be 5 digits!";
 }
+
 // Clears errors on empty form (FIX LATER)
 if(!isset($client_id_entry) && !isset($client_name_entry)){
     unset($errors);
