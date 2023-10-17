a7768e7f94aa1c75af54e4bb6a9105aad35e0a20
mclean1 <mclean1@cs.dal.ca>
2023-10-17 11:41:23 -0300

    Fixed HEAD issues from previous merge

--- cart.php
+++ cart.php
@@ -1,11 +1,7 @@
 <?php
 // Starts session
 session_start();
-<<<<<<< HEAD
 include "server_login.php";
-=======
-//Set variables
->>>>>>> 12ada200dd7b1c3874ddf0337041eabc7a3508e5
 ?>
 
 <!DOCTYPE html>
@@ -119,15 +115,10 @@ include "server_login.php";
                             </div>
                         </form>
                         <?php 
-<<<<<<< HEAD
                             // Set status as "Pending"
                             $status = "Pending";
                             // Insert data into lines771
                             $stmt2 = $conn->prepare("INSERT INTO purchaseorders771 (clientId771, datePO771, status771) VALUES (?, now(), ?)");
-=======
-                            // Insert data into lines771
-                            $stmt2 = $conn->prepare("INSERT clientId771, datePO771, status771 INTO purchaseorders771 VALUES (?,now(),?");
->>>>>>> 12ada200dd7b1c3874ddf0337041eabc7a3508e5
                             // Bind the variables to the prepared statement
                             $stmt2->bind_param("is", $client_id, $status);
                             // Execute the prepared statement and check if the data was inserted successfully
